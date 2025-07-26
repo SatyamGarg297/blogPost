ffa356ef154d6d4ced05e307567403a7471ec038
SatyamGarg297 <satyamgarg1672001@gmail.com>
2025-07-26 22:49:18 +0530

    Fix: updated Vite env variable for backend URL

--- frontend/src/services/blogService.js
+++ frontend/src/services/blogService.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/blogs';
+// const API_URL = 'http://localhost:5000/api/blogs';
+const API_URL = import.meta.env.VITE_BASE_URL;
 
 export const saveDraft = async (data) => {
     const response = await axios.post(`${API_URL}/save-draft`, data);
